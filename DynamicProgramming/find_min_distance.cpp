dd4fdfea2f898bc68404940c2545d5bd4057a1ef
dewyBrav <dewy.brav@gmail.com>
2023-02-01 19:59:20 +0300

    if (d[i - 1] == d[j - 1]) change len to i/j

--- DynamicProgramming/find_min_distance.cpp
+++ DynamicProgramming/find_min_distance.cpp
@@ -39,7 +39,7 @@ void minEditDistance(const std::string &s1, const std::string &s2)
         for (int j = 1; j < len2 + 1; ++j)
         {
             cost = 1;
-            if (d[len1 - 1] == d[len2 - 1])
+            if (d[i - 1] == d[j - 1])
                 cost = 0;
             auto replace = d[i - 1][j - 1] + cost;
             auto remove = d[i][j - 1] + 1;
@@ -60,9 +60,12 @@ void minEditDistance(const std::string &s1, const std::string &s2)
 
 int main()
 {
-    std::string s1{"ACGTACGTACGT"};
-    std::string s2{"AGTACCTACCGT"};
-
+    // std::string s1{"ACGTACGTACGT"};
+    // std::string s2{"AGTACCTACCGT"};
+    // std::string s1{"GCTAC"};
+    // std::string s2{"CTCA"};
+    std::string s2{"labrador"};
+    std::string s1{"gibraltar"};
     minEditDistance(s1, s2);
 
     return 0;
